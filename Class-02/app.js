b362732f4567fe8e7f29400cc620d27f28577160
hanzala4t2 <hanzala4t2@gmail.com>
2026-07-12 22:44:35 +0500

    Variables

---
+++ Class-02/app.js
@@ -0,0 +1,12 @@
+alert("Hi! This is my JS class 02");
+
+let name = "Muhammad Hanzala";
+let age = '21';
+let course = "Modern web application";
+let city = "Karachi";
+let country = "Pakistan";
+console.log(name, age, course, city, country)
+
+let language = "HTML";
+language = "JavaScript";
+console.log(language);
