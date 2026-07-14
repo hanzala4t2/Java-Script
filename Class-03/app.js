970fa50413a188b67abb291475c58a8e9308d10e
hanzala4t2 <hanzala4t2@gmail.com>
2026-07-14 23:39:36 +0500

    math expression

---
+++ Class-03/app.js
@@ -0,0 +1,15 @@
+let age = 21;
+let marks = 276;
+let salary = 30250;
+let price = 1010;
+let year = 2026;
+console.log(age, marks, salary, price, year);
+
+// legal variables
+let legal = "name, _user, $price";
+console.log(legal);
+
+// illegal variables
+let illegal = "1stname, first name, user-name";
+console.log(illegal);
+
