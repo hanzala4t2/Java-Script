56c092b5a76216a83f02dc7f81ae0fe754b324be
hanzala4t2 <hanzala4t2@gmail.com>
2026-07-16 01:34:19 +0500

    math expressions

---
+++ Class-04/app.js
@@ -0,0 +1,26 @@
+//POST INCREAMENT
+let num = 10;
+let newNum = num++ ;
+console.log(newNum);
+console.log(num);
+
+// POST DECREAMENT
+let count = 20;
+let newCount = count-- ;
+console.log(newCount);
+console.log(count);
+
+//PRE INCREAMENT
+let age = 21;
+let newAge = ++age;
+// console.log(newAge);
+console.log(age);
+
+//PRE DECREAMENT
+let marks = 95;
+let newMark = --marks;
+// console.log(newMark);
+console.log(marks);
+
+
+
