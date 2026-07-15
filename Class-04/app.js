1b946e757f916e8f894f1ee3cf363858e489d176
hanzala4t2 <hanzala4t2@gmail.com>
2026-07-16 02:07:05 +0500

    math expressions

--- Class-04/app.js
+++ Class-04/app.js
@@ -1,26 +1,41 @@
 //POST INCREAMENT
 let num = 10;
-let newNum = num++ ;
-console.log(newNum);
+num++;
 console.log(num);
+
 
 // POST DECREAMENT
 let count = 20;
-let newCount = count-- ;
-console.log(newCount);
+count--;
 console.log(count);
 
 //PRE INCREAMENT
-let age = 21;
-let newAge = ++age;
-// console.log(newAge);
+let age = 25;
+++age;
 console.log(age);
 
 //PRE DECREAMENT
 let marks = 95;
-let newMark = --marks;
-// console.log(newMark);
+--marks;
 console.log(marks);
 
+// Question 5
+let x = 5;
+console.log(x++);
+console.log(x);
 
+// Question 6
+console.log(10 + 5 * 2);
+  
+// Question 7
+console.log((10 + 5) * 2);
 
+// Bonus Question
+let num1 = 25;
+let num2 = 5;
+
+console.log("Addition:", num1 + num2);        
+console.log("Subtraction:", num1 - num2);      
+console.log("Multiplication:", num1 * num2);   
+console.log("Division:", num1 / num2);         
+console.log("Modulus:", num1 % num2);          
