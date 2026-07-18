3db8dbf998c1e2f855398d430a31d838db8f09f5
hanzala4t2 <hanzala4t2@gmail.com>
2026-07-18 16:24:08 +0500

    concatenation

---
+++ Class-05/app.js
@@ -0,0 +1,27 @@
+// Question 01
+let firstName = "Muhammad";
+let lastName = "Hanzala";
+let fullName = (firstName + " " + lastName);
+console.log(fullName);
+
+// Question 02
+let cityName = "I live in Karachi";
+cityName = "I am in Islamabad right now";
+console.log(cityName);
+
+// Question 03
+// let userName = prompt("Enter your name");
+// console.log("Welcome", userName);
+
+
+
+// Question 04
+// let firstnAme = prompt("Enter your firstname");
+// let lastnAme = prompt("Enter your lastname");
+// let fullnAme = (firstnAme + " " + lastnAme)
+// console.log(fullnAme);
+
+// Question 05
+// let favColor = prompt("Favorite color")
+// console.log(favColor);
+
